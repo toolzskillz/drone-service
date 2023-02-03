88a867ff246e6e3b450dd2a7b32407a29fd5549f
Iyare I. Diagboya <toolz.skillz@gmail.com>
2023-02-03 22:42:06 +0100

    Update RegExPatternUtil.java

--- src/main/java/dev/iyare/service/drone/utils/RegExPatternUtil.java
+++ src/main/java/dev/iyare/service/drone/utils/RegExPatternUtil.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 
 public class RegExPatternUtil
 {
-	public static String MED_NAME = "[a-zA-Z0-9\\s.-[._]]+"; // only letters, numbers, ‘-‘, ‘_’
-	public static String MED_CODE = "[A-Z0-9\\s._]+"; // only upper case letters, underscore and numbers’
+	public static String MED_NAME = "[a-zA-Z0-9\\s.-[._]]+"; // allowed only letters, numbers, ‘-‘, ‘_’
+	public static String MED_CODE = "[A-Z0-9\\s._]+"; // allowed only upper case letters, underscore and numbers’
 
 	public static boolean matchMedName(String input)
 	{
