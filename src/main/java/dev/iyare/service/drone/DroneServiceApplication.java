7e5fab5ea42524f7bbdb9c777a3c9b70f8e586c0
Iyare I. Diagboya <toolz.skillz@gmail.com>
2023-02-03 12:39:32 +0100

    refactor: renamed project to drone-service

--- src/main/java/dev/iyare/service/drone/DroneServiceApplication.java
+++ src/main/java/dev/iyare/service/drone/DroneServiceApplication.java
@@ -1,5 +1,8 @@
 package dev.iyare.service.drone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -18,6 +21,7 @@ import dev.iyare.service.drone.enums.Drone;
 import dev.iyare.service.drone.enums.DroneState;
 import dev.iyare.service.drone.repositories.EntityDroneRepository;
 import dev.iyare.service.drone.repositories.EntityMedicationRepository;
+import dev.iyare.service.drone.utils.StringUtil;
 
 @EntityScan("dev.iyare.service.*")
 @Configuration
@@ -57,18 +61,9 @@ public class DroneServiceApplication extends SpringBootServletInitializer
 			{
 				logger.info("+++++++++++++++++ Preloading required data in db");
 
-				EntityDrone entityDrone = null;
+				preLoadDrones();
 
-				entityDrone = new EntityDrone();
-				entityDrone.setSerial_number("1234");
-				entityDrone.setModel(Drone.LIGHT.getModel());
-				entityDrone.setWeight_limit(Drone.LIGHT.getWeightLimit());
-				entityDrone.setBattery_capacity("50");
-				entityDrone.setState(DroneState.IDLE.getDescription());
-				entityDroneRepository.save(entityDrone);
-
-				
-//				entityMedicationRepository.save(null);
+				preLoadMedications();
 
 			}
 
@@ -78,6 +73,56 @@ public class DroneServiceApplication extends SpringBootServletInitializer
 				logger.info("+++++++++++++++++ ServletContextListener destroyed");
 			}
 		};
+	}
+
+	protected void preLoadMedications()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void preLoadDrones()
+	{
+		EntityDrone entityDrone = null;
+
+		List<EntityDrone> drones = new ArrayList<EntityDrone>();
+
+		entityDrone = new EntityDrone();
+		entityDrone.setSerial_number(StringUtil.generateUniqueId(7));
+		entityDrone.setModel(Drone.LIGHT.getModel());
+		entityDrone.setWeight_limit(Drone.LIGHT.getWeightLimit());
+		entityDrone.setBattery_capacity("25");
+		entityDrone.setState(DroneState.IDLE.getDescription());
+		drones.add(entityDrone);
+		
+		entityDrone = new EntityDrone();
+		entityDrone.setSerial_number(StringUtil.generateUniqueId(7));
+		entityDrone.setModel(Drone.MIDDLE.getModel());
+		entityDrone.setWeight_limit(Drone.MIDDLE.getWeightLimit());
+		entityDrone.setBattery_capacity("30");
+		entityDrone.setState(DroneState.DELIVERING.getDescription());
+		drones.add(entityDrone);
+		
+		entityDrone = new EntityDrone();
+		entityDrone.setSerial_number(StringUtil.generateUniqueId(7));
+		entityDrone.setModel(Drone.CRUISER.getModel());
+		entityDrone.setWeight_limit(Drone.CRUISER.getWeightLimit());
+		entityDrone.setBattery_capacity("50");
+		entityDrone.setState(DroneState.LOADED.getDescription());
+		drones.add(entityDrone);
+		
+		entityDrone = new EntityDrone();
+		entityDrone.setSerial_number(StringUtil.generateUniqueId(7));
+		entityDrone.setModel(Drone.HEAVY.getModel());
+		entityDrone.setWeight_limit(Drone.HEAVY.getWeightLimit());
+		entityDrone.setBattery_capacity("70");
+		entityDrone.setState(DroneState.RETURNING.getDescription());
+		drones.add(entityDrone);
+		
+	
+
+		entityDroneRepository.saveAll(drones);
+
 	}
 
 }
